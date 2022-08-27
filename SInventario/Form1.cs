874bf5653e8923429fe5d5c7ed1dd4e7b8b453e5
cmontellanob <carlosmontellano@gmail.com>
2022-08-27 07:24:20 -0400

    primer commit

---
+++ SInventario/Form1.cs
@@ -0,0 +1,52 @@
+using System.Data.SqlClient;
+
+namespace SInventario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Nombre SErvidor BD
+            string DataSource = ".";
+
+            //nombre Base de datos
+            string InitialCatalog = "bd_inventario";
+
+            //datos de conexion
+            string UserID = "sa";
+            string Password = "Univalle123$";
+
+
+            string connectionString = "Data Source =" + DataSource
+                + "; Initial Catalog =" + InitialCatalog
+                + "; User ID=" + UserID
+                + "; Password=" + Password
+                ;
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                lblEstado.Text = "Nos hemos conectado a la BD" + connection.State.ToString();
+
+                
+            }
+            catch (Exception ex)
+            {
+                lblEstado.Text ="A ocurrido un problema \n" + ex.Message;
+            }
+
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
